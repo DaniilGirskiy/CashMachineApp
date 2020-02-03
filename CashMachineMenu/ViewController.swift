5977bf7690700c6cf3310a04bf945e416c29632d
DaniilGirskiy <https://github.com/DaniilGirskiy>
2020-02-03 18:06:20 +0300

    Initial Commit

---
+++ CashMachineMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CashMachineMenu
+//
+//  Created by Danya on 03/02/2020.
+//  Copyright © 2020 Daniil Girskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
